cf20208c246896927f1adc01e96dab5477453572
Kirushield <elankeethan@edu.univ-eiffel.fr>
2023-12-07 11:00:10 +0100

    commit

--- Prog2.py
+++ Prog2.py
@@ -30,28 +30,13 @@ lstBords = [(x1,y1), (x2,y1), (x2,y2), (x1,y2)]
 while True:
     if type_ev(donne_ev()) == "Quitte":
         break
-    ev = donne_ev()
     efface("joueur")
+    ev = donne_ev()
     if ev is not None:
         if type_ev(ev) == "Touche":
-            # Sauvegarde les anciennes coordonnées
-            oldX, oldY = xJoueur, yJoueur
+            pass
 
-            # Déplace le joueur selon les touches flèche
-            if touche(ev) == "Up":
-                yJoueur -= tailleJoueur
-            elif touche(ev) == "Down":
-                yJoueur += tailleJoueur
-            elif touche(ev) == "Left":
-                xJoueur -= tailleJoueur
-            elif touche(ev) == "Right":
-                xJoueur += tailleJoueur
-
-            # Vérifie les limites du rectangle
-            if xJoueur < x1 or xJoueur > x2 or yJoueur < y1 or yJoueur > y2:
-                # Si les nouvelles coordonnées sont en dehors, restaure les anciennes
-                xJoueur, yJoueur = oldX, oldY
-            joueur(xJoueur, yJoueur)
+        joueur
 
     mise_a_jour()
 
