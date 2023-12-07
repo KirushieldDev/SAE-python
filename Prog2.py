2c2b937ba297af6c5d2f1ae78f81b3c820baa470
Kirushield <elankeethan@edu.univ-eiffel.fr>
2023-12-07 11:08:23 +0100

    Version 2

--- Prog2.py
+++ Prog2.py
@@ -2,10 +2,7 @@ from fltk import *
 
 def joueur(x: int, y: int, taille=5):
     """Dessine le joueur"""
-    cercle(x, y, taille, couleur="lime", tag="joueur")
-
-def dessin(ax: int, ay: int, bx: int, by: int):
-    ligne(ax, ay, bx, by, couleur="lime", tag="dessin")
+    cercle(x, y, taille, couleur="lime")
 
 largeurFenetre = 1500
 hauteurFenetre = 1000
@@ -28,16 +25,31 @@ lstBords = [(x1,y1), (x2,y1), (x2,y2), (x1,y2)]
 
 # Boucle principale pour traiter les événements
 while True:
-    if type_ev(donne_ev()) == "Quitte":
-        break
-    efface("joueur")
     ev = donne_ev()
     if ev is not None:
         if type_ev(ev) == "Touche":
-            pass
+            # Sauvegarde les anciennes coordonnées
+            oldX, oldY = xJoueur, yJoueur
 
-        joueur
+            # Déplace le joueur selon les touches flèche
+            if touche(ev) == "Up":
+                yJoueur -= tailleJoueur
+            elif touche(ev) == "Down":
+                yJoueur += tailleJoueur
+            elif touche(ev) == "Left":
+                xJoueur -= tailleJoueur
+            elif touche(ev) == "Right":
+                xJoueur += tailleJoueur
+
+            # Vérifie les limites du rectangle
+            if xJoueur < x1 or xJoueur > x2 or yJoueur < y1 or yJoueur > y2:
+                # Si les nouvelles coordonnées sont en dehors, restaure les anciennes
+                xJoueur, yJoueur = oldX, oldY
+
+            # Efface la fenêtre et redessine le joueur
+            efface_tout()
+            rectangle(0, 0, largeurFenetre, hauteurFenetre, remplissage="black")
+            rectangle(x1, y1, x2, y2, couleur="white")
+            joueur(xJoueur, yJoueur)
 
     mise_a_jour()
-
-ferme_fenetre()
