0a5c1784e55b3dca313ba3672380df51135d529b
Kirushield <kirushield@gmail.com>
2023-12-30 09:53:13 +0100

    Commit

--- Prog2.py
+++ Prog2.py
@@ -6,10 +6,10 @@ invincible = False
 
 
 def joueur(x: int, y: int, taille=5):
-    if not invincible:
+    if invincible:
         cercle(x, y, taille, couleur="lime", tag="joueur")
     else:
-        cercle(x, y, taille, couleur="red", tag="joueur")
+        cercle(x, y, taille, couleur="cyan", tag="joueur")
 
 
 def dessin(ax: int, ay: int, bx: int, by: int):
